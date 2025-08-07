31fa202b5e63c6d8ab47ac27aedbf8e99716f5e0
Ina <inatkata@yahoo.com>
2025-08-07 14:59:41 +0200

    Details.cshtml

--- BountyHuntersBlog.Web/Models/ViewModels/MissionDetailsViewModel.cs
+++ BountyHuntersBlog.Web/Models/ViewModels/MissionDetailsViewModel.cs
@@ -1,26 +1,29 @@
 ﻿using BountyHuntersBlog.Models.Domain;
 using BountyHuntersBlog.Models.ViewModels;
 
-public class MissionDetailsViewModel
+namespace BountyHuntersBlog.Models.ViewModels
 {
-    public Guid Id { get; set; }
-    public string Title { get; set; }
-    public string PageTitle { get; set; }
-    public string Content { get; set; }
-    public string FeaturedImageUrl { get; set; }
-    public string ShortDescription { get; set; }
-    public DateTime MissionDate { get; set; }
-    public string UrlHandle { get; set; }
+    public class MissionDetailsViewModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string ShortDescription { get; set; }
+        public DateTime MissionDate { get; set; }
+        public string UrlHandle { get; set; }
 
-    public Hunter Author { get; set; }
+        public Hunter Author { get; set; }
 
-    public int TotalLikes { get; set; }
-    public bool Liked { get; set; }
+        public int TotalLikes { get; set; }
+        public bool Liked { get; set; }
 
-    public List<MissionCommentViewModel> Comments { get; set; } = new();
+        public List<MissionCommentViewModel> Comments { get; set; } = new();
 
-    public bool Visible { get; set; }
+        public bool Visible { get; set; }
 
-    // This is for adding a new comment
-    public string CommentDescription { get; set; }
+        // This is for adding a new comment
+        public string CommentDescription { get; set; }
+    }
 }
