fe9051c8fc4bc21a077ccc69cb2c3b51637524a4
Ina <inatkata@yahoo.com>
2025-08-13 22:59:05 +0200

    some views to adjust

--- BountyHuntersBlog.ViewModels/Account/LoginViewModel.cs
+++ BountyHuntersBlog.ViewModels/Account/LoginViewModel.cs
@@ -3,7 +3,7 @@
     public class LoginViewModel
     {
         [System.ComponentModel.DataAnnotations.Required]
-        public string Email { get; set; } = null!;   
+        public string Username { get; set; } = null!;   
 
         [System.ComponentModel.DataAnnotations.Required,
          System.ComponentModel.DataAnnotations.DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
